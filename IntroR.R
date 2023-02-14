3c58c2ffab1cb4fd6ff3fbfa09752ecf4099f165
beneducizachary <zzb0018@auburn.edu>
2023-02-14 16:56:33 -0600

    Commit for 14 Feb 2023.

--- IntroR.R
+++ IntroR.R
@@ -83,3 +83,49 @@ df$value[df$name == "Jie"]
 # easier way
 
 subset(df, name == "Jie")
+
+#### Installing packages ####
+
+# Functions not installed in base R are available in packages
+
+install.packages("ggplot2")
+install.packages("ggpubr")
+
+# Dependencies are needed for many packages
+
+# Need to load package into your environment with library()
+
+library(ggplot2)
+library(ggpubr)
+
+#### Importing data into R ####
+
+# Can copy and provide a file path OR make sure your file is in the working directory
+datum <- read.csv("BMP_Veg_Data_2022_1_19_2023.csv")
+head(datum)
+
+#### Data visualization with the preloaded mtcars in R ####
+data("mtcars")
+
+# Call the object to see. Can also use the function head() to call the first few observations
+mtcars
+
+# Can make a basic plot
+plot(x = mtcars$wt, y = mtcars$mpg)
+
+# CHange the labels of the axes, font style, and point shape
+plot(x = mtcars$wt, y = mtcars$mpg,
+     xlab = "Car weight",
+     ylab = "Miles per gallon",
+     font.lab = 6, # Changes the font style
+     pch = 20, #pch changes the shape of the points
+     )
+# Ggplot layers are built upon by adding "geomes" or layers
+# Convention is to tstart a new geome on the next line
+# Ggplot works with layers. The layers towards the bottom are at the front
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point(alpha = 0.5) + # Alpha adjusts the transparency
+  geom_smooth(method = lm, color = "black") + # can easily use the options method to avoid this smoothing and apply a linear model
+  xlab("Weight") +
+  ylab("Miles per gallon") +
+  theme_classic() # Can apply themes with buil in visual styles
