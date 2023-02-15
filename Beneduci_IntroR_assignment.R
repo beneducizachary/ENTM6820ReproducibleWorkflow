9f751b9e07497dfad651093c3835102506d115db
beneducizachary <zzb0018@auburn.edu>
2023-02-14 20:25:22 -0600

    Questions 1-2

---
+++ Beneduci_IntroR_assignment.R
@@ -0,0 +1,37 @@
+### Introduction to R assignment ###
+
+## Question 1 ##
+# Code can be entered in the console (not saved)
+# or in a script (to be saved).
+# The output of the code is typically found in the console,
+# although outputs in the form of objects will be stored
+# in the global environment.
+# Plots show up in the "Plots" tab found at the bottom
+# right of the RStudio window.
+# The global environment is the area where R objects
+# are stored.
+# You can go to the console and type "help()" and
+# insert the function of interest into the parentheses.
+# An R package is a collection of pre-made functions
+# that can be downloaded and used.
+# A function is a collection of code used to manipulate objects
+# You navigate to the "Packages" tab in the lower right
+# window to see all available and loaded functions.
+# The working directory is the directory where the R project
+# is saved, and we can locate it by running the "getwd()" function.
+
+## Question 2 ##
+# In RStudio, go to file and select "New Project".
+# Select "Version Control" under "Create a Project".
+# Select "Git" as the version control type. 
+# If you do not have a Git repository,
+# navigate to your browser and login to GitHub.
+# Navigate to the "Repositories" tab and click the "New" icon.
+# Give the repository a name and select your desired license.
+# Inside your new repositoru, select the green "Code" icon
+# and copy the url of your repository.
+# Paste this url into the Git project wizard, name the local copy of your
+# repository, and specify the file path where you would like
+# it to be locally stored.
+# Your R project should now be linked with Git.
+
