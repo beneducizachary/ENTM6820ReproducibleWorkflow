dba695cef5e60d140cbcb34296a64bd7465ebb85
beneducizachary <zzb0018@auburn.edu>
2023-02-20 17:27:56 -0600

    First R assignment Feb 20, 2023

--- Beneduci_IntroR_assignment.R
+++ Beneduci_IntroR_assignment.R
@@ -1,6 +1,6 @@
-### Introduction to R assignment ###
+##### Introduction to R assignment #####
 
-## Question 1 ##
+### Question 1 ###
 # Code can be entered in the console (not saved)
 # or in a script (to be saved).
 # The output of the code is typically found in the console,
@@ -20,7 +20,7 @@
 # The working directory is the directory where the R project
 # is saved, and we can locate it by running the "getwd()" function.
 
-## Question 2 ##
+### Question 2 ###
 # In RStudio, go to file and select "New Project".
 # Select "Version Control" under "Create a Project".
 # Select "Git" as the version control type. 
@@ -35,6 +35,87 @@
 # it to be locally stored.
 # Your R project should now be linked with Git.
 
-## Questions 3 ##
+### Questions 3 ###
 
-# A vector is an object of multiple scalar elements (i.e. numbers)
+# A vector is an object of multiple scalar elements (i.e. numbers). It can have
+# multiple rows but only one column.
+# A matrix is an object that contains only one data class, such as numeric or
+# character. A matrix could be considered (at least visually) as being made of
+# multiple vectors. A matrix can have multiple rows AND columns.
+# A dataframe is an object containing multiple data classes. It can be thought
+# of as a matrix with different data classes between columns.
+
+### Question 4 ###
+
+z <- 1:200
+
+mean(z)
+sd(z)
+
+t <- z > 1
+
+df <- data.frame(z, t)
+
+colnames(df) <- c("z", "zlog")
+
+df$zsquared <- (df$z)^2
+
+df_subset <- subset(df, (zsquared > 10) & (zsquared < 100))
+
+### Question 5 ###
+
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("purrr")
+install.packages("lme4")
+install.packages("emmeans")
+
+library(ggplot2)
+library(dplyr)
+library(purrr)
+library(lme4)
+library(emmeans)
+
+data(iris) # Load the iris dataset for manipulation via packages
+head(iris)
+
+## ggplot2
+p <- ggplot(data=iris, aes(Sepal.Length, Sepal.Width))
+p + geom_point() # scatterplot of Sepal.Width ~ Sepal.Length
+
+## dplyr
+
+iris <- as_tibble(iris) # Convert to tibble
+
+iris %>% arrange(Sepal.Width) # Observations in ascending order by Sepal.Width
+
+## purr
+
+means <- 1:3
+
+randsamp <- map(means, rnorm, n = 3, sd = .5) # Use map function to generate 
+# three random samples of 3 subsamples
+
+str(randsamp)
+
+## lme4
+
+data("sleepstudy") # Actually, the iris dataset doesn't have an adequate number of levels
+# in "Species" to run a random effect, so I'll use the preloaded lme4 dataset
+
+head(sleepstudy)
+
+sleepstudy$Subject = as.factor(sleepstudy$Subject)
+
+m1 <- lmer(Reaction ~ Days + (1|Subject), sleepstudy, REML = TRUE)
+
+summary(m1)
+
+## emmeans
+
+emmeans(m1, ~Days)
+
+tips_data <- read.csv("https://raw.githubusercontent.com/noelzach/Reproducibility/c29492f2d6e6b36544ee42dfe7a630814ac137cb/06_IntroR/Assignment/TipsR.csv",
+                      na.strings = ".")
+
+tips_data # Note NA values under "Gender"
