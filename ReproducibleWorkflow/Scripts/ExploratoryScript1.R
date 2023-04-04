cf964dad087b4c79d9a550bb4b7de1bbea41a3ad
BuildTools <unconfigured@null.spigotmc.org>
2023-04-03 22:37:29 -0500

    More tests.

--- ReproducibleWorkflow/Scripts/ExploratoryScript1.R
+++ ReproducibleWorkflow/Scripts/ExploratoryScript1.R
@@ -7,7 +7,6 @@ str(datum1)
 ### Load packages
 install.packages("tidyverse")
 library(tidyverse)
-library(dplyr)
 
 ### Format for analysis of planted wildflower richness and density.
 datum2 <- datum1 %>%
@@ -44,61 +43,156 @@ datum2 <- datum1 %>%
  # Finally, we'll add the treatment information. However, the treatments were
   # not applied at this stage, so we are looking for no treatment effects!
   mutate(Treatment =
-           case_when(Subplot == 1 & Site == FCU1 ~ "Disking",
-                     Subplot == 2 & Site == FCU1 ~ "Mowing",
-                     Subplot == 3 & Site == FCU1 ~ "Combo",
-                     Subplot == 4 & Site == FCU1 ~ "Control",
-                     Subplot == 5 & Site == FCU1 ~ "Mowing",
-                     Subplot == 6 & Site == FCU1 ~ "Control",
-                     Subplot == 7 & Site == FCU1 ~ "Disking",
-                     Subplot == 8 & Site == FCU1 ~ "Combo",
-                     Subplot == 9 & Site == FCU1 ~ "Combo",
-                     Subplot == 10 & Site == FCU1 ~ "Disking",
-                     Subplot == 11 & Site == FCU1 ~ "Mowing",
-                     Subplot == 12 & Site == FCU1 ~ "Control",
-                     Subplot == 13 & Site == FCU1 ~ "Control",
-                     Subplot == 14 & Site == FCU1 ~ "Mowing",
-                     Subplot == 15 & Site == FCU1 ~ "Combo",
-                     Subplot == 16 & Site == FCU1 ~ "Disking",
-                     Subplot == 1 & Site == BEU1 ~ "Combo",
-                     Subplot == 2 & Site == BEU1 ~ "Disking",
-                     Subplot == 3 & Site == BEU1 ~ "Mowing",
-                     Subplot == 4 & Site == BEU1 ~ "Control",
-                     Subplot == 5 & Site == BEU1 ~ "Mowing",
-                     Subplot == 6 & Site == BEU1 ~ "Control",
-                     Subplot == 7 & Site == BEU1 ~ "Combo",
-                     Subplot == 8 & Site == BEU1 ~ "Disking",
-                     Subplot == 9 & Site == BEU1 ~ "Disking",
-                     Subplot == 10 & Site == BEU1 ~ "Mowing",
-                     Subplot == 11 & Site == BEU1 ~ "Control",
-                     Subplot == 12 & Site == BEU1 ~ "Combo",
-                     Subplot == 13 & Site == BEU1 ~ "Control",
-                     Subplot == 14 & Site == BEU1 ~ "Combo",
-                     Subplot == 15 & Site == BEU1 ~ "Disking",
-                     Subplot == 16 & Site == BEU1 ~ "Mowing",
-                     Subplot == 1 & Site == PBU1 ~ "Mowing",
-                     Subplot == 2 & Site == PBU1 ~ "Disking",
-                     Subplot == 3 & Site == PBU1 ~ "Combo",
-                     Subplot == 4 & Site == PBU1 ~ "Control",
-                     Subplot == 5 & Site == PBU1 ~ "Control",
-                     Subplot == 6 & Site == PBU1 ~ "Combo",
-                     Subplot == 7 & Site == PBU1 ~ "Mowing",
-                     Subplot == 8 & Site == PBU1 ~ "Disking",
-                     Subplot == 9 & Site == PBU1 ~ "Disking",
-                     Subplot == 10 & Site == PBU1 ~ "Mowing",
-                     Subplot == 11 & Site == PBU1 ~ "Combo",
-                     Subplot == 12 & Site == PBU1 ~ "Control",
-                     Subplot == 13 & Site == PBU1 ~ "Disking",
-                     Subplot == 14 & Site == PBU1 ~ "Combo",
-                     Subplot == 15 & Site == PBU1 ~ "Control",
-                     Subplot == 16 & Site == PBU1 ~ "Mowing"))
+           case_when(Subplot == 1 & Site == "FCU1" ~ "Disking",
+                     Subplot == 2 & Site == "FCU1" ~ "Mowing",
+                     Subplot == 3 & Site == "FCU1" ~ "Combo",
+                     Subplot == 4 & Site == "FCU1" ~ "Control",
+                     Subplot == 5 & Site == "FCU1" ~ "Mowing",
+                     Subplot == 6 & Site == "FCU1" ~ "Control",
+                     Subplot == 7 & Site == "FCU1" ~ "Disking",
+                     Subplot == 8 & Site == "FCU1" ~ "Combo",
+                     Subplot == 9 & Site == "FCU1" ~ "Combo",
+                     Subplot == 10 & Site == "FCU1" ~ "Disking",
+                     Subplot == 11 & Site == "FCU1" ~ "Mowing",
+                     Subplot == 12 & Site == "FCU1" ~ "Control",
+                     Subplot == 13 & Site == "FCU1" ~ "Control",
+                     Subplot == 14 & Site == "FCU1" ~ "Mowing",
+                     Subplot == 15 & Site == "FCU1" ~ "Combo",
+                     Subplot == 16 & Site == "FCU1" ~ "Disking",
+                     Subplot == 1 & Site == "BEU1" ~ "Combo",
+                     Subplot == 2 & Site == "BEU1" ~ "Disking",
+                     Subplot == 3 & Site == "BEU1" ~ "Mowing",
+                     Subplot == 4 & Site == "BEU1" ~ "Control",
+                     Subplot == 5 & Site == "BEU1" ~ "Mowing",
+                     Subplot == 6 & Site == "BEU1" ~ "Control",
+                     Subplot == 7 & Site == "BEU1" ~ "Combo",
+                     Subplot == 8 & Site == "BEU1" ~ "Disking",
+                     Subplot == 9 & Site == "BEU1" ~ "Disking",
+                     Subplot == 10 & Site == "BEU1" ~ "Mowing",
+                     Subplot == 11 & Site == "BEU1" ~ "Control",
+                     Subplot == 12 & Site == "BEU1" ~ "Combo",
+                     Subplot == 13 & Site == "BEU1" ~ "Control",
+                     Subplot == 14 & Site == "BEU1" ~ "Combo",
+                     Subplot == 15 & Site == "BEU1" ~ "Disking",
+                     Subplot == 16 & Site == "BEU1" ~ "Mowing",
+                     Subplot == 1 & Site == "PBU1" ~ "Mowing",
+                     Subplot == 2 & Site == "PBU1" ~ "Disking",
+                     Subplot == 3 & Site == "PBU1" ~ "Combo",
+                     Subplot == 4 & Site == "PBU1" ~ "Control",
+                     Subplot == 5 & Site == "PBU1" ~ "Control",
+                     Subplot == 6 & Site == "PBU1" ~ "Combo",
+                     Subplot == 7 & Site == "PBU1" ~ "Mowing",
+                     Subplot == 8 & Site == "PBU1" ~ "Disking",
+                     Subplot == 9 & Site == "PBU1" ~ "Disking",
+                     Subplot == 10 & Site == "PBU1" ~ "Mowing",
+                     Subplot == 11 & Site == "PBU1" ~ "Combo",
+                     Subplot == 12 & Site == "PBU1" ~ "Control",
+                     Subplot == 13 & Site == "PBU1" ~ "Disking",
+                     Subplot == 14 & Site == "PBU1" ~ "Combo",
+                     Subplot == 15 & Site == "PBU1" ~ "Control",
+                     Subplot == 16 & Site == "PBU1" ~ "Mowing")) %>%
+  mutate_at(c(16), as.factor) %>%
+  mutate(Treatment = relevel(Treatment, "Control"))
+### ANALYSIS
 
-### Preliminary model fitting
+# This dataset represents pre-treatment data collected during the 2022 field season
+# before treatments were applied in fall 2022/winter 2023. Therefore,
+# there should not be any treatment level effects. However, there may be effects of location
+# (spatial autocorrelation) in this dataset that may erroneously produce treatment effects.
 
-# Let's try a simple linear model:
-lm1 <- lm()
+## Let's look at the data:
+
+ggplot(datum2, aes(x = Richness)) + geom_histogram()
+
+ggplot(datum2, aes(x = TotalDens)) + geom_histogram()
+
+# We can see that the distribution of the raw data, especially for density, is not normal.
+# There is also likely underdispersion in the richness response.
+
+## Let's try a general linear model anyway:
+lmRich <- lm(Richness~Treatment, datum2)
+lmTotalDens <- lm(TotalDens~Treatment, datum2)
+
+summary(lmRich) # No treatment effects here, so doesn't look like treatments allocation was not skewed.
+summary(lmTotalDens) # No significance, but there is weak evidence suggesting that the mowing treatments
+                     # have 1.21 more flowers on average. This is a pretty small effect, however, considering
+                     # a subplot can have hundreds of flowers.
+plot(residuals(lmRich))
+plot(residuals(lmTotalDens))
+hist(residuals(lmRich))
+hist(residuals(lmTotalDens))
+
+# Messy residuals plots... showing that there's clearly clustering of values indicating autocorrelation.
+# Of course, knowing that each subplot was sampled four times throughout the season, we have pseudoreplication
+# for the variable of interest (Treatment). Additionally, there are three Sites, each with 16 subplots
+# each (4 of each Treatment group). Therefore, we also (probably) have spatial autocorrelation.
+
+## Let's fix the pseudoreplication problem.
+library(lme4)
+
+lmmRich <- lmer(Richness ~ Treatment + (1|Site/Subplot), datum2)
+summary(lmmRich)
+plot(residuals(lmmRich))
+
+lmmTotalDens <- lmer(TotalDens ~ Treatment + (1|Site/Subplot), datum2)
+summary(lmmTotalDens)
+plot(residuals(lmmTotalDens))
+
+# The nested random effect explicitly specifies that there are multiple measurements per subplot,
+# and so should apply the correct degrees of freedom to test for Treatment differences. However,
+# we still have messy residuals. The density model is also having trouble converging (singular fit).
+
+## Switch to generalized linear mixed models for a better error distribution:
+# Try a poisson error family first:
+glmmRich <- glmer(Richness~Treatment + (1|Site/Subplot), family = poisson, datum2)
+summary(glmmRich)
+plot(residuals(glmmRich)) # Having another singular fit issue. This time for richness. 
+
+glmmTotalDens <- glmer(TotalDens ~ Treatment + (1|Site/Subplot), family = poisson, datum2)
+summary(glmmTotalDens)
+plot(residuals(glmmTotalDens)) # This improved a bit. Residuals at lower values are still iffy
+                               # and there is a perponderance of residuals below zero with a portion
+                               # of high values.
+                               # A negative binomial error family may help, as it can handle some 
+                               # overdispersion and excess zeros compared to poisson.
+
+glmmRich2 <- glmer.nb(Richness ~ Treatment + (1|Site/Subplot), datum2)
+summary(glmmRich2)
+
+glmmTotalDens2 <- glmer.nb(TotalDens ~ Treatment + (1|Site/Subplot), datum2)
+plot(residuals(glmmTotalDens2))
+
+### glmmTMB
 
 
+## Try a multivariable model with Site and Round:
+
+lmRich2 <- lm(Richness~Treatment+Site+Round, datum2)
+lmTotalDens2 <- lm(TotalDens~Treatment+Site+Subplot, datum2)
+
+summary(lmRich2)
+summary(lmTotalDens2)
+
+plot(residuals(lmRich2))
+plot(residuals(lmTotalDens2))
+
+# Didn't fix anything. Really the issue here is that the error distributrion appears non-Gaussian,
+# pseudoreplication is present, and spatial autocorrelation is present.
+
+# Let's run a generalized linear model testing poisson error family:
+
+glmRich <- glm(Richness~Treatment, family = poisson, datum2)
+glmTotalDens <- glm(TotalDens~Treatment, family = poisson, datum2)
+
+summary(glmRich)
+summary(glmTotalDens)
+
+plot(residuals.glm(glmRich))
+plot(residuals.glm(glmTotalDens))
+
+# Run the same for negative binomial error family:
+
+glmRich2 <- glm(Richness~Treatment, family = negbin, datum2)
 
 ## Tile plot of planted richness across subplots
 ggplot(datum2, aes(x = Col, y = Row, fill = Richness)) +
